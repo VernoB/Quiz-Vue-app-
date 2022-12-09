8be240fb0dc43826892017ba44fa98e3c50b6be3
VernoB <mbagna3@gmail.com>
2022-12-09 14:15:32 +0400

    Implementation of compnent

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <script setup>
+import CardComoponent from './components/Card.vue'
 import q from './data/quizes.json';
 import {ref, watch} from 'vue'
 
@@ -21,13 +22,14 @@ watch(search, cb => {
         <input v-model.trim="search" type="text" placeholder="Search...">
       </header>
       <div class="options-container">
-        <div v-for="quize in quizes" :key="quize.id" class="card">
+        <CardComoponent v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+        <!-- <div v-for="quize in quizes" :key="quize.id" class="card">
           <img :src="quize.img" :alt="quize.name">
           <div class="card-text">
             <h2>{{quize.name}}</h2>
             <p>{{quize.questions.length}} questions</p>
           </div>
-        </div>
+        </div> -->
       </div>
   </div>
 </template>
@@ -62,29 +64,6 @@ header input {
   display : flex;
   flex-wrap: wrap;
   margin-top: 50px ;
-}
-
-.card{
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-  margin-bottom: 35px;
- margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img{
-  width: 100%;
-  height: 113px;
-  margin: 0%;
-}
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
 }
 
 </style>
